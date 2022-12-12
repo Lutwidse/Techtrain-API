e93e6d6bb097db40257631780603fbcd182132ed
Lutwidse <lutwidse@m0n4.com>
2022-12-13 08:46:06 +0900

    add sql login info as const variable

--- config.go
+++ config.go
@@ -1,12 +1,5 @@
 package techtrain_api
 
-import "database/sql"
-
-func ConnectToDb() (*sql.DB, error) {
-	db, errdb := sql.Open("mysql", "root:$A!c6i7xC!93%@FZ@localhost:3306/techtrain_db")
-	if errdb != nil{
-		return nil, errdb
-	}
-	err := db.Ping()
-	return db, err
-}
+const (
+	SqlArgs = "root:$A!c6i7xC!93%@FZ@/techtrain_db"
+)
