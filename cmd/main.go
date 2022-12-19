d148962c1ccb245dc887c1b2063a841a967ca082
Lutwidse <lutwidse@m0n4.com>
2022-12-20 05:27:50 +0900

    add sql.yaml to store login credentials

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+
 	api "github.com/Lutwidse/Techtrain-API"
 	"github.com/jinzhu/gorm"
+	yaml "gopkg.in/yaml.v2"
 )
 
+type SQL struct {
+	User string `yaml:"user"`
+	Pw   string `yaml:"pw"`
+	Db   string `yaml:"db"`
+}
+
 func main() {
-	db, err := gorm.Open("mysql", "root:$A!c6i7xC!93%@FZ@/techtrain_db")
+	file, err := ioutil.ReadFile("../cmd/config/sql.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sql SQL
+
+	err = yaml.Unmarshal(file, &sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	loginInfo := (sql.User + ":" + sql.Pw + "/" + sql.Db)
+
+	db, err := gorm.Open("mysql", loginInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
